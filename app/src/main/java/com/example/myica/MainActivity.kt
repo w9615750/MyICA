d96490891948b6b3a49566d307a285223c3183b6
w9615750 <W9615750@live.tees.ac.uk>
2023-10-24 17:17:30 +0100

    changed the text

--- app/src/main/java/com/example/myica/MainActivity.kt
+++ app/src/main/java/com/example/myica/MainActivity.kt
@@ -41,6 +41,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     MyICATheme {
-        Greeting("Android")
+        Greeting("Shivani")
     }
 }
